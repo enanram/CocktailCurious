982c5df68cc7a055b337e9d75f66ed641c40d16f
lhf633 <lhf633@users.noreply.github.com>
2021-03-22 11:46:23 +0000

    Updated BrowseRecipeRecyclerAdapter and BrowseRecipesFragment.kt to actually show card views that can be changed based on what's passed through ViewHolders
    Updated BrowseRecipes, YourMenu & Favourites fragments to include recyclerViews, with the functionality in each relevant class to also display cards.
    
    Began work on CocktailRecipe class to draw data from into recyclerView lists.

--- app/src/main/java/com/k2eb/cocktailcurious/CocktailRecipe.kt
+++ app/src/main/java/com/k2eb/cocktailcurious/CocktailRecipe.kt
@@ -2,11 +2,16 @@ package com.k2eb.cocktailcurious
 
 class CocktailRecipe(
     private var cocktailName: String,
-    private var cocktailDescription: String,
+    private var cocktailBlurb: String,
     private var cocktailRating: Int
 ) {
+    lateinit var cocktailDescription: String
     var isFavourite: Boolean = false
     var cocktailIngredients = arrayListOf<Ingredient>()
+
+    private fun addDescription(description: String){
+        cocktailDescription = description
+    }
 
     private fun toggleFavourite(): Boolean {
         this.isFavourite = !isFavourite

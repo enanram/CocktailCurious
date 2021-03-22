11ae900ae3fc1f0b45e0d2c959da50c0729ddc4e
lhf633 <lhf633@users.noreply.github.com>
2021-03-22 13:36:46 +0000

    Updated BrowseRecipeRecyclerAdapter and BrowseRecipesFragment.kt to actually show card views that can be changed based on what's passed through ViewHolders
    Updated BrowseRecipes, YourMenu & Favourites fragments to include recyclerViews, with the functionality in each relevant class to also display cards.
    
    Began work on CocktailRecipe class to draw data from into recyclerView lists.

---
+++ app/src/main/java/com/k2eb/cocktailcurious/BrowseRecipesRecyclerAdapter.kt
@@ -0,0 +1,46 @@
+package com.k2eb.cocktailcurious
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BrowseRecipesRecyclerAdapter(
+    private var cocktails: List<CocktailRecipe>
+) :
+    RecyclerView.Adapter<BrowseRecipesRecyclerAdapter.RecipeViewHolder>() {
+
+    lateinit var mcxt: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        mcxt = parent.context
+        val inflater = LayoutInflater.from(mcxt)
+        val view: View = inflater.inflate(R.layout.fragment_browse_card, parent, false)
+        return RecipeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+
+        holder.tvName.text = cocktails[position].cocktailName
+        holder.tvBlurb.text = cocktails[position].cocktailBlurb
+
+    }
+
+    override fun getItemCount(): Int {
+        return cocktails.size
+    }
+
+    class RecipeViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var ivImage: ImageView = itemView.findViewById(R.id.cocktail_image)
+        var tvName: TextView = itemView.findViewById(R.id.cocktail_name)
+        var tvBlurb: TextView = itemView.findViewById(R.id.cocktail_blurb)
+        var ratRatingBar: RatingBar = itemView.findViewById(R.id.cocktail_rating)
+
+    }
+
+}

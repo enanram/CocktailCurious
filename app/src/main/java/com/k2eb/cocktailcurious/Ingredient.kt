889eaf31d63017b47197245e6ab115d0d2073de7
enanram <dm_92@live.co.uk>
2021-03-24 20:11:23 +0000

    messed everything up but added parcelable to CocktailRecipe

--- app/src/main/java/com/k2eb/cocktailcurious/Ingredient.kt
+++ app/src/main/java/com/k2eb/cocktailcurious/Ingredient.kt
@@ -1,4 +1,28 @@
 package com.k2eb.cocktailcurious
 
-open class Ingredient(var name: String) {
+import android.os.Parcel
+import android.os.Parcelable
+
+open class Ingredient(var name: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Ingredient> {
+        override fun createFromParcel(parcel: Parcel): Ingredient {
+            return Ingredient(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Ingredient?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 }

10f1ccb49f7a668ed36a1438d3acc5005097a34a
Grant Scullion <grantscullion@hotmail.co.uk>
2021-03-25 17:28:45 +0000

    changes to VirtualCupboardFragment.kt

--- app/src/main/java/com/k2eb/cocktailcurious/VirtualCupboardFragment.kt
+++ app/src/main/java/com/k2eb/cocktailcurious/VirtualCupboardFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 
 
 class VirtualCupboardFragment : Fragment() {
@@ -68,6 +70,52 @@ class VirtualCupboardFragment : Fragment() {
    // fun setCupboardView (view : View) {
     // }
 
+    fun setCupboardView(view: View) {
+        val btnBigAdd = view.findViewById<Button>(R.id.btn_big_add)
+        val txvPrompt = view.findViewById<TextView>(R.id.txv_prompt)
+        val btnSmallAdd = view.findViewById<Button>(R.id.btn_small_add)
+        val btnLetsGo = view.findViewById<Button>(R.id.btn_lets_go)
+        val btnClearAll = view.findViewById<Button>(R.id.btn_clear_all)
+        val rvIngredients = view.findViewById<RecyclerView>(R.id.rv_ingredients)
+        if (cupboardList.size == 0) {
+            //findViewById(R.id.btn_big_add) setVisbility (View.VISIBLE)
+            btnBigAdd.isClickable = true
+            btnBigAdd.visibility = View.VISIBLE
+            //findViewById(R.id.txv_prompt) setVisbility (View.VISIBLE)
+            txvPrompt.visibility = View.VISIBLE
+            //findViewById(R.id.btn_small_add) setVisbility (View.INVISIBLE)
+            btnSmallAdd.isClickable = false
+            btnSmallAdd.visibility = View.INVISIBLE
+            //findViewById(R.id.btn_lets_go) setVisbility (View.INVISIBLE)
+            btnLetsGo.isClickable = false
+            btnLetsGo.visibility = View.INVISIBLE
+            //findViewById(R.id.btn_clear_all) setVisbility (View.INVISIBLE)
+            btnClearAll.isClickable = false
+            btnClearAll.visibility = View.INVISIBLE
+            //findViewById(R.id.rv_ingredients) setVisbility (View.INVISIBLE)
+            rvIngredients.isClickable = false
+            rvIngredients.visibility = View.INVISIBLE
+        } else {
+            //view.findViewById<Button>(R.id.btn_big_add) setVisbility (View.INVISIBLE)
+            btnBigAdd.isClickable = false
+            btnBigAdd.visibility = View.INVISIBLE
+            //view.findViewById<TextView>(R.id.txv_prompt) setVisbility (View.INVISIBLE)
+            txvPrompt.visibility = View.INVISIBLE
+            //view.findViewById<Button>(R.id.btn_small_add) setVisbility (View.VISIBLE)
+            btnSmallAdd.isClickable = true
+            btnSmallAdd.visibility = View.VISIBLE
+            //view.findViewById<Button>(R.id.btn_lets_go) setVisbility (View.VISIBLE)
+            btnLetsGo.isClickable = true
+            btnLetsGo.visibility = View.VISIBLE
+            //view.findViewById<Button>(R.id.btn_clear_all) setVisbility (View.VISIBLE)
+            btnClearAll.isClickable = true
+            btnClearAll.visibility = View.VISIBLE
+            //view.findViewById<Button>(R.id.rv_ingredients) setVisbility (View.VISIBLE)
+            rvIngredients.isClickable = true
+            rvIngredients.visibility = View.VISIBLE
+        }
+    }
+
     // TODO method to add/remove ingredients to cupboard database
 
     /**
@@ -91,6 +139,14 @@ class VirtualCupboardFragment : Fragment() {
         cupboardList.remove(ingredient)
         if (cupboardList.size == 0) {
             isEmpty = true
+        }
+    }
+
+    fun onClickClearAll() {
+        if (cupboardList.isEmpty()) {
+            Toast.makeText(this,"Your cupboard is already empty", Toast.LENGTH_SHORT).show()
+        } else {
+            cupboardList.clear()
         }
     }
 

b01fb4003d4ae63e594ff56c6dc26de04a147118
lhf633 <lhf633@users.noreply.github.com>
2021-03-29 20:21:03 +0100

    Ingredient Recycler

---
+++ app/src/main/java/com/k2eb/cocktailcurious/CupboardRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.k2eb.cocktailcurious
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class CupboardRecyclerAdapter(
+        private var ingredients: List<Ingredient>
+): RecyclerView.Adapter<CupboardRecyclerAdapter.IngredientViewHolder>() {
+
+    lateinit var mcxt: Context
+
+    override fun getItemCount(): Int {
+        return ingredients.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        mcxt = parent.context
+        val inflater = LayoutInflater.from(mcxt)
+        val view: View = inflater.inflate(R.layout.fragment_virtual_cupboard_card, parent, false)
+        return IngredientViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        holder.tvName.text = ingredients[position].name
+
+        holder.ivTrash.setOnClickListener {
+                VirtualCupboardFragment.removeIngredient(ingredients[position])
+            }
+        }
+
+
+    class IngredientViewHolder(itemView: View) :
+            RecyclerView.ViewHolder(itemView) {
+        var cardView: CardView = itemView.findViewById(R.id.card_cupboard)
+        var tvName: TextView = itemView.findViewById(R.id.cupboard_ingredient)
+        var ivTrash: Button = itemView.findViewById(R.id.cupboard_trash)
+    }
+}
